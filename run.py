5de42c5947fc02735f00009175ebe598903bd4d8
Zsolnai Csaba <kanesoban@gmail.com>
2020-06-27 15:15:22 +0200

    Removed current and voltage scaling

--- run.py
+++ run.py
@@ -1,45 +1,41 @@
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 
 # milliseconds
 SIM_TIME = 200
-# ampers
-CURRENT_SCALE = 1
-CONST_CURRENT = 7 * CURRENT_SCALE
-VOLTAGE_SCALE = 1
+# milliampers
+CONST_CURRENT = 7
 MIN_POTENTIAL = -90
+RESTING_POTENTIAL = -70
 CLAMP_VOLTAGES = False
-dt = 1
+dt = 0.5
 sim_length = int(SIM_TIME / dt)
 
 input_current = np.ones(sim_length) * CONST_CURRENT
+input_current[:200] = 0
+input_current[-300:] = 0
 
 a = 0.02
 b = 0.2
-c = -65 * VOLTAGE_SCALE
+c = -65
 d = 8
-threshold_potential = 30
+SPIKING_THRESHOLD = 30
 
 # Simulate single neuron
 
 v = np.empty(sim_length)
 u = np.empty(sim_length)
-v[0] = c
+v[0] = RESTING_POTENTIAL
 u[0] = b * v[0]
 
 for t in range(1, sim_length):
-    if v[t - 1] >= threshold_potential:
+    if v[t - 1] >= SPIKING_THRESHOLD:
         v[t] = c
         u[t] = u[t - 1] + d
     else:
         v[t] += 0.5 * (0.04 * v[t - 1] ** 2 + 5 * v[t - 1] + 140 - u[t - 1] + input_current[t - 1]) * dt
         if CLAMP_VOLTAGES:
-            v[t] = min(threshold_potential, v[t])
-            v[t] = max(MIN_POTENTIAL, v[t])
-        v[t] += 0.5 * (0.04 * v[t - 1] ** 2 + 5 * v[t - 1] + 140 - u[t - 1] + input_current[t - 1]) * dt
-        if CLAMP_VOLTAGES:
-            v[t] = min(threshold_potential, v[t])
+            v[t] = min(SPIKING_THRESHOLD, v[t])
             v[t] = max(MIN_POTENTIAL, v[t])
         u[t] += a * (b * v[t - 1] - u[t - 1]) * dt
         if CLAMP_VOLTAGES:
